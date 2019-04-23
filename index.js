39c5c034156c1284be6ebb508bc860d9a5f0f2ef
yuicer <xiejunhao@bytedance.com>
2019-04-23 12:09:02 +0800

    perf(frame): finish the main func for stage 1

--- index.js
+++ index.js
@@ -19,35 +19,18 @@ module.exports = (options, ctx) => ({
 
   plugins: [
     ['@vuepress/active-header-links', options.activeHeaderLinks],
-    '@vuepress/plugin-blog',
+    [
+      '@vuepress/plugin-blog',
+      {
+        pageEnhancers: [
+          {
+            when: ({ regularPath }) => regularPath === '/',
+            frontmatter: { layout: 'Categories' || 'Layout' }
+          }
+        ]
+      }
+    ],
     '@vuepress/search',
-    '@vuepress/plugin-nprogress',
-    [
-      'container',
-      {
-        type: 'tip',
-        defaultTitle: {
-          '/zh/': '提示'
-        }
-      }
-    ],
-    [
-      'container',
-      {
-        type: 'warning',
-        defaultTitle: {
-          '/zh/': '注意'
-        }
-      }
-    ],
-    [
-      'container',
-      {
-        type: 'danger',
-        defaultTitle: {
-          '/zh/': '警告'
-        }
-      }
-    ]
+    '@vuepress/plugin-nprogress'
   ]
 })
