ba04c4df82c6cd3d2884459c0988537fb69c2121
yuicer <xiejunhao@bytedance.com>
2019-05-09 21:58:54 +0800

    feat(leran): add new plugins & start using lerna

--- packages/vuepress-theme-yuicer/index.js
+++ packages/vuepress-theme-yuicer/index.js
@@ -1,19 +1,7 @@
 const path = require('path')
-const moment = require('moment')
 
 // Theme API.
 module.exports = (options, ctx) => ({
-  // 增加一个时间参数到 $page
-  extendPageData($page) {
-    const {
-      lastUpdated,
-      frontmatter: { date }
-    } = $page
-    const time = date || lastUpdated || Date.now()
-    $page.time = time
-  },
-  // 注入一个排序好的全局计算属性
-  enhanceAppFiles: path.resolve(__dirname, 'plugins/plugin-sorted-pages/enhanceAppFile.js'),
   // 增加一个默认主页
   additionalPages: [
     {
@@ -41,15 +29,8 @@ module.exports = (options, ctx) => ({
     '@vuepress/plugin-nprogress',
     '@vuepress/back-to-top',
     ['@vuepress/medium-zoom', { selector: '.page img' }],
-    [
-      '@vuepress/last-updated',
-      {
-        transformer: (timestamp, lang) => {
-          moment.locale(lang)
-          return moment(new Date(timestamp))
-        }
-      }
-    ],
+    '@yuicer/sorted-pages',
+    // require(path.resolve(__dirname, './plugins/plugin-sorted-pages')),
     [
       '@vuepress/google-analytics',
       {
